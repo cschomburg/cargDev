cc7905a5bd4bd7a4db197031c9dc4c946f7c1ddb
Constantin <xconstruct@gmail.com>
2009-07-23 22:59:31 +0200

    first commit

---
+++ core.lua
@@ -0,0 +1,65 @@
+--[[
+	cargDebug
+]]
+local addon = CreateFrame("ScrollingMessageFrame", nil, UIParent)
+local _G = getfenv()
+
+function addon:new()
+	self:SetTimeVisible(300)
+	self:SetInsertMode("BOTTOM")
+	self:SetFrameStrata("HIGH")
+	self:SetJustifyH("LEFT")
+	self:SetHeight(540)
+	self:SetWidth(400)
+	self:SetMaxLines(24)
+	self:SetPoint("TOPLEFT", UIParent, 10, -10)
+	self:SetFontObject(ChatFontNormal)
+	self:Show()
+end
+addon:new()
+
+SlashCmdList['DEVCLEAR'] = function(...) return addon:Clear() end
+SLASH_DEVCLEAR1 = '/devclear'
+SLASH_DEVCLEAR2 = '/dcl'
+
+addon.debug = function(...)
+	local strText
+	for i=1, select("#", ...) do
+		if(strText) then strText = strText..", " else strText = "" end
+		strText = strText..(select(i, ...) or "|cffff9090nil|r")
+	end
+	addon:AddMessage(strText)
+end
+
+--[[*****************************************
+	Reloads the interface
+*******************************************]]
+SlashCmdList['RELOADUI'] = function(...) ReloadUI() end
+SLASH_RELOADUI1 = '/rl'
+
+--[[*****************************************
+	Prints the name of the hovered frame and tries to Spew the entered command (use "frame" for the hovered frame)
+*******************************************]]
+SlashCmdList['FRAME'] = function(msg)
+	frame = GetMouseFocus()
+	if(SlashCmdList.SPEW and msg and strlen(msg)>0) then
+		local firstLetter = msg:sub(1, 1)
+		if(firstLetter == ":" or firstLetter == ".") then
+			SlashCmdList.SPEW("frame"..msg)
+		else
+			SlashCmdList.SPEW(msg)
+		end
+	else
+		addon.debug(frame:GetName())
+	end
+end
+SLASH_FRAME1 = '/frame'
+
+--[[*****************************************
+	Collects garbage
+*******************************************]]
+SlashCmdList['GARBAGE'] = function() collectgarbage("collect") end
+SLASH_GARBAGE1 = '/gcl'
+
+_G['debug'] = addon.debug
+_G['cargDebug'] = addon
